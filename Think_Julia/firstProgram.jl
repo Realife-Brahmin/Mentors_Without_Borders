29b18165d3afcf34d016a28dd3418969a0234d2e
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-01-23 12:15:06 -0800

    basic print statements with variables.

--- Think_Julia/firstProgram.jl
+++ Think_Julia/firstProgram.jl
@@ -3,8 +3,8 @@ include("functions/rightJustify.jl")
 rightJustify("monty");
 name = "Aryan Ritwajeet Jha";
 rightJustify(name);
-# age = 24;
-# str = "Hello! My name is  " * name * " and I'm " * age * " years old.\n Nice to meet you.";
+age = 24;
+str = "Hello! My name is  $(name) and I'm  $(age) years old. Nice to meet you.";
+strSmall = str[1:61]
 # println(str)
-# rightJustify("Hello! My name is"* name * " and I'm " * age *" years old. 
-# Nice to meet you.")
+rightJustify(strSmall)
