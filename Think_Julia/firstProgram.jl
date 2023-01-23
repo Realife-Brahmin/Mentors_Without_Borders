f5d672791d20eea0fc2bd38ec4781f7b1c018693
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-01-23 12:20:30 -0800

    nice colours :hot_face:

--- Think_Julia/firstProgram.jl
+++ Think_Julia/firstProgram.jl
@@ -6,5 +6,11 @@ rightJustify(name);
 age = 24;
 str = "Hello! My name is  $(name) and I'm  $(age) years old. Nice to meet you.";
 strSmall = str[1:61]
-# println(str)
 rightJustify(strSmall)
+
+# using printstyled
+
+# printing text in different colors
+for color in [:red, :cyan, :blue, :magenta]
+	printstyled("Hello World $(color)\n"; color = color)
+end
