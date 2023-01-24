17551a3d6d8d113ae40db2f00758b0845650407d
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-01-24 15:02:48 -0800

    Ex 3-4 printgrid (generalized of course :smile_cat:) added.
    This book really has some good exercises.

---
+++ Think_Julia/functions/printgrid.jl
@@ -0,0 +1,11 @@
+function printgrid(boxWidth, boxHeight, numRows, numCols)
+    strBaseHorz = "+ " * "- "^boxWidth;
+    strLineHorz = strBaseHorz^numRows * "+\n";
+    strBaseVert = "|" * " "^(2*boxWidth + 1);
+    strLineVert = strBaseVert^numRows * "|\n";
+    strBox = strLineHorz* strLineVert^boxHeight;
+    strBoxes = strBox^numCols * strLineHorz
+    println(strBoxes);
+end
+
+# printgrid(1, 7, 5, 3)
