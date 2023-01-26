e85652d0ef4ce0a10bce0b21e750272573476f10
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-01-25 23:52:10 -0800

    polygon, arc, circle refactored into polyline

--- Think_Julia/functions/turtlesFromThinkJulia.jl
+++ Think_Julia/functions/turtlesFromThinkJulia.jl
@@ -2,46 +2,36 @@ using ThinkJulia
 
 🐢 = Turtle()
 
-function square(t, a)
+function polyline(t, n, a, angle)
+    numSides = trunc(n*angle/360);
     @svg begin
-        for i in 1:4
+        for i = 1:numSides
             forward(t, a)
-            turn(t, -90)
+            turn(t, -360/n)
+        end
+        truncatedAngle = mod(angle, trunc(360/n));
+        if truncatedAngle != 0
+            lastLen = (truncatedAngle*n*a)/360;
+            forward(t, lastLen)
         end
     end
 end
 
 function polygon(t, n, a)
-    @svg begin
-        for i in 1:n
-            forward(t, a)
-            turn(t, -360/n)
-        end
-    end
-end
-
-function circle(t , r)
-    circumference = 2*π*r;
-    n = trunc(circumference/3) + 3;
-    a = 2*π*r/n;
-    n = trunc(a / 3) + 3
-    polygon(t, n, a);
+    polyline(t, n, a, 360);
 end
 
 function arc(t, r, angle)
     circumference = 2π*r;
     n = trunc(circumference/3) + 3;
-    a = 2*π*r/n;
-    numSides = trunc(n*angle/360);
-    @svg begin
-        for i in 1:numSides
-            forward(t, a)
-            turn(t, -360/n)
-        end
-    end
+    a = circumference/n;
+    polyline(t, n, a, angle)
+end
+
+function circle(t , r)
+    arc(t, r, 360);
 end
 
 🐫 = Turtle()
-arc(🐫, 100, 330)
 
-
+circle(🐫, 150)
