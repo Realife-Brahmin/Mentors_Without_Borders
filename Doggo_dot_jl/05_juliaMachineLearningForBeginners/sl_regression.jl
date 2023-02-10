e0881af6918557f01c18b31ce1787bc71240e953
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-02-09 21:19:05 -0800

    plot limits generalized.

--- Doggo_dot_jl/05_juliaMachineLearningForBeginners/sl_regression.jl
+++ Doggo_dot_jl/05_juliaMachineLearningForBeginners/sl_regression.jl
@@ -4,18 +4,22 @@ using CSV, GLM, Plots, TypedTables
 rawDataFolder = "rawData/";
 rawDataFilename = rawDataFolder*"housingdata.csv";
 data1 = CSV.File(rawDataFilename)
-X = data1.size
-Y = round.(Int, data1.price / 1000)
-table1 = Table(X = X, Y = Y)
+houseSize = data1.size
+housePrice = round.(Int, data1.price / 1000)
+table1 = Table(X = houseSize, Y = housePrice)
 
-gr(size = (600, 600));
+gr(size = (500, 500));
 
-p_scatter = scatter(X, Y,
-    xlims = (0, 5000),
-    ylims = (0, 800),
+p_scatter = scatter(houseSize, housePrice,
+    xlims = (minimum(houseSize)*0.9, maximum(houseSize)*1.1),
+    ylims = (minimum(housePrice)*0.9, maximum(housePrice)*1.1),
     xlabel = "Size (sqft)",
     ylabel = "Price in 1000\$",
     title = "Housing Prices in Portland",
     legend = false,
     color = :red
 )
+
+###Use GLM package for Linear Regression Model
+#ols = Ordinary Least Squares
+ols = lm(@formula(Y ~ X), table1)   
