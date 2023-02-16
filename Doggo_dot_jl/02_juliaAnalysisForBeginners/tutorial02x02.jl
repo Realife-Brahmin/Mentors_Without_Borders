3edc754e6ec6f0299136e8ab1df2df8571ebc5fc
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-02-15 16:51:22 -0800

    m

--- Doggo_dot_jl/02_juliaAnalysisForBeginners/tutorial02x02.jl
+++ Doggo_dot_jl/02_juliaAnalysisForBeginners/tutorial02x02.jl
@@ -4,15 +4,21 @@ P_demand(Q) = (-Q + 32) / 2
 using CairoMakie
 
 scene, layout = layoutscene(resolution = (325, 325))
-scene
+# scene
 
 ax = layout[1, 1] = Axis(scene,
     xlabel = "Quantity", xlabelsize = 10,
     xticklabelsize = 9, xticksize = 3,
     ylabel = "Price", ylablesize = 10,
     yticklabelsize = 9, yticksize = 3,
-    ytickformat = "\${:d}",
+    ytickformat = "\${:f}",
     title = "Supply and Demand", titlesize = 12
 )
 
+Q_range = 1:25
+
+lineobject1 = lines!(ax, Q_range, P_supply, linewidth = 1, color = :blue)
+lineobject2 = lines!(ax, Q_range, P_demand, linewidth = 1, color = :red)
+
 scene
+
