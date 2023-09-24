cec90d568efd5a9f642a76d708060b946846f7d3
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2023-09-24 13:15:02 -0700

    Update functions.jl

--- Think_Julia/src/functions.jl
+++ Think_Julia/src/functions.jl
@@ -1,5 +1,28 @@
-println("Hello, World!")
-parse(Int64, "32")
-parse(Float64, "3.14159")
-parse(Int64, "Hello")
-trunc(Int64, 3.9999)
+function printlyrics()
+    println("I'm a lumberjack, and I'm okay.")
+    println("I sleep all night and I work all day.")
+end
+
+function repeatlyrics()
+    printlyrics()
+    printlyrics()
+end
+
+repeatlyrics()
+
+function printtwice(bruce)
+    println(bruce)
+    println(bruce)
+end
+
+printtwice(π)
+# "Spam"
+"Spam"^4 # * concatenates two strings
+printtwice("Spam "^4)
+
+function cattwice(part1, part2)
+    part1 = string(part1)
+    part2 = string(part2)
+    concat = part1 * part2
+    printtwice(concat)
+end
