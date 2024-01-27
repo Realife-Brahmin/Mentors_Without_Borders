aaa68f6867e693d79afa73cb34943f769bba0b87
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-01-27 13:41:51 -0800

    uploading here

---
+++ Think_Julia/testFIle1.jl
@@ -0,0 +1,7 @@
+function printlyrics() # void functions
+    println("I'm a lumberjack, and I'm okay.")
+    println("I sleep all night and I work all day.")
+end
+
+#type Int64, FLoat64, String
+printlyrics()
---
+++ Think_Julia/testFIle1.py
@@ -0,0 +1,6 @@
+def printlyrics()
+    print("I'm a lumberjack, and I'm okay.")
+    print("I sleep all night and I work all day.")
+
+
+printlyrics()
