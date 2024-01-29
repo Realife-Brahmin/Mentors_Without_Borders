9a96d30d627e8c36f988722d3219de03551ac7c6
entonuina <entonuina@gmail.com>
2024-01-29 22:27:02 +0100

    Data type of printlyrics function was add

--- Think_Julia/testFIle1.jl
+++ Think_Julia/testFIle1.jl
@@ -1,6 +1,9 @@
 function printlyrics() # void functions
-    println("I'm a lumberjack, and I'm okay.")
-    println("I sleep all night and I work all day.")
+    introduction="I'm a lumberjack, and I'm okay."
+    sleep_work="I sleep all night and I work all day."
+    #tells the data type useed in the function 
+    println(typeof(introduction)," ", introduction)
+    println(typeof(sleep_work)," ", sleep_work)
 end
 
 #type Int64, FLoat64, String
--- Think_Julia/testFIle1.py
+++ Think_Julia/testFIle1.py
@@ -1,6 +1,4 @@
-def printlyrics()
+def printlyrics():
     print("I'm a lumberjack, and I'm okay.")
     print("I sleep all night and I work all day.")
-
-
 printlyrics()
