bcf0f5abc24790f9e699629e31de9e2aa4c1ef5b
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-03-11 03:34:06 -0700

    adding these handy functions although they don' thave a pupose rn

--- Think_Julia/src/HelperFunctions.jl
+++ Think_Julia/src/HelperFunctions.jl
@@ -4,3 +4,23 @@ function myprintln(verbose::Bool, args...)
     end
 end;
 
+"""
+    myzeros(array)
+
+Create a new array with the same size and type as the given `array`, but initialized with zeros.
+"""
+function myzeros(array)
+    return zeros(eltype(array), size(array))
+end
+
+"""
+    myfill(array, value)
+
+Create a new array with the same size and type as the given `array`, but initialized with `value`.
+The type of the returned array's elements is preserved from the original array.
+"""
+function myfill(array, value)
+    T = eltype(array)
+    return fill(T(value), size(array))
+end
+
