f73482c58d141b31dc8ac5ce94fb444419f64562
Aryan Ritwajeet Jha <aryan.r.jha@gmail.com>
2024-03-11 05:48:27 -0700

    waiting for doc to be generated, so already creating a script for the next chapter

---
+++ Think_Julia/src/tuples.jl
@@ -0,0 +1,9 @@
+# tuples.jl
+
+
+include("setup.jl")
+
+include("./arrays.jl") # for speed comparision with array implementations
+include("./dictionaries.jl")
+include("./functions.jl")
+include("./strings.jl")
