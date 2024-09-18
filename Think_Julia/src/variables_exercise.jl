39a82422207c0de7553614c72efd4cd2e795c52a
Janice Kinyua <janicekinyua0@gmail.com>
2024-09-18 08:58:44 +0300

    solved answer for question 2 by finding total cost of books

--- Think_Julia/src/variables_exercise.jl
+++ Think_Julia/src/variables_exercise.jl
@@ -1,3 +1,12 @@
+# question 1 exercise 2.3
 r = 5
 v = 4/3*π*r^3
 println("volume of sphere of radius $r is $v")
+
+# question 2 exercise 2.3
+coverprice =24.95
+discount_percent= 40
+shipping_1_copy = 3
+shipping_rest = 0.75
+total_copies = 60
+total_cost = coverprice*(100-discount_percent)/100*total_copies + shipping_1_copy*1 +shipping_rest*(total_copies-1)
